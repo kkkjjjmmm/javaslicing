f9e8744c11f7b67945badd9c79bada9397c47bd4
kkkjjjmmm <574780854@qq.com>
2018-07-31 21:33:36 +0100

    new shelljava

---
+++ WALA-START/shelljava.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo "---------start---------"
+# ./shelljava.sh abc def
+# $2 -> def
+OUTPUT_FOLDER="parser_output"
+TARGET="$1"
+FILENAME="$(basename $1)"
+LIBS="src/main/java/kkkjjjmmm/slicer/Util.java"
+DESTINATION="src/main/java/kkkjjjmmm/modified/${FILENAME}"
+
+MAINCLASS=$2 # "Lkkkjjjmmm/test/Example"
+SRCCALLER=$3 # "SliceL"
+SRCCALLEE=$4 # "fake"
+
+rm -r $OUTPUT_FOLDER
+mkdir $OUTPUT_FOLDER
+mkdir -p "$OUTPUT_FOLDER/kkkjjjmmm/slicer"
+cp $LIBS "$OUTPUT_FOLDER/kkkjjjmmm/slicer/Util.java"
+
+java -jar doparser.jar $TARGET $OUTPUT_FOLDER
+
+cd $OUTPUT_FOLDER
+find . -name "*.java" | xargs javac  
+jar -cvf transformed.jar *
+
+echo "--------finished-------"
+
